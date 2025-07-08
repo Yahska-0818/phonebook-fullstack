532eb98c078537a5d3b48c6b6be5f6a4d636ab25
Yahska-0818 <101645835+Yahska-0818@users.noreply.github.com>
2025-07-08 13:25:39 +0000

    added express.static for frontend to work

--- index.js
+++ index.js
@@ -14,6 +14,8 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time :cont
 
 app.use(cors())
 
+app.use(express.static('dist'))
+
 let persons = [
     { 
         "id": "1",
