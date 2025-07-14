58139338d5769088d3356540b848e34e3475f9d7
Yahska-0818 <101645835+Yahska-0818@users.noreply.github.com>
2025-07-14 06:21:41 +0000

    Implemented mongoDB database

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+require('dotenv').config()
 const express = require('express')
-const app =  express()
 const morgan = require('morgan')
+const Contact = require('./models/contact')
 
 morgan.token('content', function (req, res) { return JSON.stringify(req.body) })
+
+const app =  express()
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time :content', {
@@ -11,34 +14,12 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time :cont
      }
 }))
 
-
 app.use(express.static('dist'))
 
-let persons = [
-    { 
-        "id": "1",
-        "name": "Arto Hellas", 
-        "number": "040-123456"
-    },
-    { 
-        "id": "2",
-        "name": "Ada Lovelace", 
-        "number": "39-44-5323523"
-    },
-    { 
-        "id": "3",
-        "name": "Dan Abramov", 
-        "number": "12-43-234345"
-    },
-    { 
-        "id": "4",
-        "name": "Mary Poppendieck", 
-        "number": "39-23-6423122"
-    }
-]
-
 app.get('/api/persons',(request,response) => {
-    response.json(persons)
+    Contact.find({}).then(contacts => {
+    response.json(contacts)
+    })
 })
 
 app.get('/info',(request,response)=> {
@@ -49,39 +30,64 @@ app.get('/info',(request,response)=> {
     `);
 })
 
-app.get('/api/persons/:id',(request,response)=> {
-    const id = request.params.id
-    const person = persons.find(person => person.id === id)
-    response.json(person)
+app.get('/api/persons/:id', (request, response,next) => {
+    Contact.findById(request.params.id)
+        .then(contact => {
+            if (contact) {
+                response.json(contact)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id',(request,response)=> {
-    const id = request.params.id
-    const person = persons.filter(person => person.id !== id)
-    response.status(204).end()
+    Contact.findByIdAndDelete(request.params.id)
+    .then(result => {
+        response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons',(request,response)=>{
     let person = request.body
     if (!person.name || !person.number) {
-        return response.status(200).json({ 
+        return response.status(400).json({ 
             error: 'content missing' 
         })
     }
-
-    namesList = persons.map(person=>person.name)
     
-    if (namesList.includes(person.name)) {
-        return response.status(200).json({ 
-            error: 'name must be unique' 
-        })
-    }
+    const contact = new Contact({
+        name: person.name,
+        number: person.number
+    })
 
-
-    person.id = Math.floor(Math.random()*10**6)
-    persons.push(person)
-    response.json(person)
+    contact.save().then(savedPerson=>{
+        response.json(savedPerson)
+    })
 })
+
+app.put('/api/persons/:id',(request,response)=>{
+    const {name,number} = request.body
+    Contact.findOneAndUpdate({name},{$set:{number}},{returnDocument:'after'})
+    .then(updatedPerson=>{
+        response.json(updatedPerson)
+    })
+    .catch(error=>next(error))
+})
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
