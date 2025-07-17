4933684baff29340051db975eec96eb61e50d1d2
Yahska-0818 <101645835+Yahska-0818@users.noreply.github.com>
2025-07-17 13:13:35 +0000

    Implemented validation for name and number

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.delete('/api/persons/:id',(request,response)=> {
     .catch(error => next(error))
 })
 
-app.post('/api/persons',(request,response)=>{
+app.post('/api/persons',(request,response,next)=>{
     let person = request.body
     if (!person.name || !person.number) {
         return response.status(400).json({ 
@@ -63,12 +63,14 @@ app.post('/api/persons',(request,response)=>{
         number: person.number
     })
 
-    contact.save().then(savedPerson=>{
+    contact.save()
+    .then(savedPerson=>{
         response.json(savedPerson)
     })
+    .catch(error=>next(error))
 })
 
-app.put('/api/persons/:id',(request,response)=>{
+app.put('/api/persons/:id',(request,response,next)=>{
     const {name,number} = request.body
     Contact.findOneAndUpdate({name},{$set:{number}},{returnDocument:'after'})
     .then(updatedPerson=>{
@@ -82,6 +84,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   } 
 
   next(error)
